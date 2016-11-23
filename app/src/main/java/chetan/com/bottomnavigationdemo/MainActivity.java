4dca5650e049423a20551e663a4c9492c1eeb2dd
chetan2991 <chetanshingare2991@gmail.com>
2016-11-23 19:08:34 +0530

    first commit

--- app/src/main/java/chetan/com/bottomnavigationdemo/MainActivity.java
+++ app/src/main/java/chetan/com/bottomnavigationdemo/MainActivity.java
@@ -1,50 +1,61 @@
 package chetan.com.bottomnavigationdemo;
 
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView mBottomNavigationView;
+    ImageView mHeaderImage;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBottomNavigationView = ( BottomNavigationView )findViewById( R.id.bottomNavigationView );
-        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
-        {
+        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        mHeaderImage = (ImageView) findViewById(R.id.headerImage);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item)
-            {
-                switch ( item.getItemId() )
-                {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
                     case R.id.item_one:
-                                        displayToast("Button One Is Click");
-                                        break;
+
+                        mHeaderImage.setImageDrawable(getImage("@drawable/menu_one_image"));
+                        displayToast("Button One Is Click");
+                        break;
 
                     case R.id.item_two:
-                                        displayToast( "Button Two Is Click");
-                                        break;
+                        mHeaderImage.setImageDrawable(getImage("@drawable/menu_two_image"));
+
+                        displayToast("Button Two Is Click");
+                        break;
 
                     case R.id.item_three:
+                        mHeaderImage.setImageDrawable(getImage("@drawable/menu_three_image"));
 
-                                        displayToast( "Button Three Is Click" );
-                                        break;
+                        displayToast("Button Three Is Click");
+                        break;
 
                 }
                 return false;
             }
         });
     }
-    private void displayToast( String msg )
-    {
-        Toast.makeText( getApplicationContext(), ""+msg, Toast.LENGTH_LONG ).show();
+
+    public Drawable getImage(String uri) {
+        int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+        Drawable res = getResources().getDrawable(imageResource);
+        return res;
+    }
+
+    private void displayToast(String msg) {
+        Toast.makeText(getApplicationContext(), "" + msg, Toast.LENGTH_LONG).show();
     }
 
 }
